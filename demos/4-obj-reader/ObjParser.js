0d11dab6c1656f2b923c9856d1ae684df213271c
Ciro S. Costa <ciro.costa@usp.br>
2015-03-15 22:05:56 -0300

    adds regexes to faces -- NEEDS REFACTOR

--- demos/4-obj-reader/ObjParser.js
+++ demos/4-obj-reader/ObjParser.js
@@ -1,13 +1,36 @@
 (function (root) {
-  root.ObjParser = {};
+  var non_null = function (val) {
+    return val != null;
+  };
+
+  var to_float = function (val) {
+    return val == '' ? undefined : parseFloat(val);
+  };
+
+  var slashed_to_array = function (val) {
+    return val.split('/').map(to_float);
+  };
+
+  // TODO: improve this .
+
+  // this is actually very expensive (doing this
+  // kind of match). We should actually take a
+  // better approach of applying the regex match
+  // after the first_letter lookup and then get
+  // the match.
+  var REGEXES = {
+    vertices: /^v\s(-?\d+\.\d+)\s(-?\d+\.\d+)\s(-?\d+\.\d+)?$/,
+    comments: /^#(.*)?/,
+    vertices_normals: /^vn\s(-?\d+\.\d+)\s(-?\d+\.\d+)\s(-?\d+\.\d+)$/,
+    faces: /^f\s(\d+)\s(\d+)\s(\d+)\s(\d+)$/,
+    faces2: /^f(\s\d+\/\d+)(\s\d+\/\d+)(\s\d+\/\d+)(\s\d+\/\d+)?$/,
+    faces3: /^f(\s\d+\/\d+?\/\d+)(\s\d+\/\d+?\/\d+)(\s\d+\/\d+?\/\d+)(\s\d+\/\d+?\/\d+)?$/,
+    faces4: /^f(\s\d+\/(\d+)?\/\d+)(\s\d+\/(\d+)?\/\d+)(\s\d+\/(\d+)?\/\d+)(\s\d+\/(\d+)?\/\d+)?$/,
+  };
 
   function parse (text) {
-    var result = {vertices: [], comments: [], vertices_normals: [], };
-    var REGEXES = {
-      vertices: /^v\s(-?\d+\.\d+)\s(-?\d+\.\d+)\s(-?\d+\.\d+)?$/,
-      comments: /^#(.*)?/,
-      vertices_normals: /^vn\s(-?\d+\.\d+)\s(-?\d+\.\d+)\s(-?\d+\.\d+)$/,
-    };
+    var result = {vertices: [], comments: [], vertices_normals: [],
+      faces: [], faces2: [], faces3: [], faces4: []};
 
     text.split('\n').forEach(function (line) {
       for (var reg in REGEXES) {
@@ -16,9 +39,16 @@
         if (!match)
           continue;
 
-        result[reg].push(match.slice(1).map(function (val) {
-          return parseFloat(val);
-        }));
+        switch (reg) {
+          case 'faces2':
+          case 'faces3':
+          case 'faces4':
+            result[reg].push(match.slice(1).filter(non_null).map(slashed_to_array));
+            break;
+          default:
+            result[reg].push(match.slice(1).map(to_float));
+        }
+
         break;
       }
     });
