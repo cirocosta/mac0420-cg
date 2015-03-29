063db8b343321ae418027ee53373a60733827434
Ciro S. Costa <ciro.costa@usp.br>
2015-03-29 15:01:27 -0300

    adds keyhandler and ortho projection

--- demos/6-cubes/index.js
+++ demos/6-cubes/index.js
@@ -1,11 +1,12 @@
-(function (root) {
+(function (root, document) {
   'use strict';
 
   const canvas = document.querySelector('canvas');
 
   let gl = WebGLUtils.setupWebGL(canvas);
   let angle = 30.0;
-  let t_matrix;
+  let eye_x = .0, eye_y = .0, eye_z = .2;
+  let near = .0, far = 2.0;
 
   const QUAD_VERTEX_SIZE = 3;
   const N_VERTICES = 3 * 3;
@@ -14,10 +15,9 @@
   Shaders.initFromElems(gl, document.getElementById('vshader'),
                             document.getElementById('fshader'))
 
+  let modelMatrix = MV.mat4.identity();;
+  let projMatrix = new Matrix4();
   let viewMatrix = new Matrix4();
-  viewMatrix.setLookAt(.2, .2, .25,
-                       .0, .0, .0,
-                       .0, 1., .0);
 
   /**
    * A benefit that comes from using buffers is
@@ -32,18 +32,18 @@
    */
   function initVertexBuffers (gl, locations) {
     const VERTICES = new Float32Array([
-      //    x ,   y ,  z, PointSize , R, G, B
-          -0.5, 0.5 ,  -0.4,  1.0, 0.0, 0.0,
+      //    x ,   y ,  z,       R,   G,   B
+           0.0, 0.5 ,  -0.4,  1.0, 0.0, 0.0,
           -0.5, -0.5,  -0.4,  0.0, 1.0, 0.0,
-           0.5, 0.5 ,  -0.4,  0.0, 0.0, 1.0,
+           0.5, -0.5,  -0.4,  0.0, 1.0, 0.0,
 
-           -0.5, 0.5 , -0.2,  1.0, 0.0, 0.0,
-          -0.5, -0.5,  -0.2,  0.0, 1.0, 0.0,
-           0.5, 0.5 ,  -0.2,  0.0, 0.0, 1.0,
+          -0.5, 0.5,  -0.2,  1.0, 0.0, 1.0,
+           0.0, -0.5 ,  -0.2,  0.0, 0.0, 1.0,
+          0.5, 0.5 ,  -0.2,  0.0, 1.0, 0.0,
 
-           -0.5, 0.5 , -0.0,  1.0, 0.0, 0.0,
-          -0.5, -0.5,  -0.0,  0.0, 1.0, 0.0,
-           0.5, 0.5 ,  -0.0,  0.0, 0.0, 1.0,
+          0.0, 0.5  ,  -0.0,  0.0, 1.0, 0.0,
+          -0.5, -0.5,  -0.0,  1.0, 0.0, 1.0,
+          0.5, -0.5 , -0.0,  0.0, 0.0, 1.0,
     ]);
     const FSIZE = VERTICES.BYTES_PER_ELEMENT;
     const STRIDE = FSIZE * (QUAD_VERTEX_SIZE + 3);
@@ -71,32 +71,73 @@
   }
 
   const LOCATIONS = Shaders.getLocations(gl,
-    ['a_Position', 'a_Color', 'u_xformMatrix', 'u_viewMatrix']);
+    ['a_Position', 'a_Color', 'u_modelMatrix', 'u_viewMatrix', 'u_projMatrix']);
   initVertexBuffers(gl, LOCATIONS);
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.enable(gl.DEPTH_TEST);
 
   function render () {
     // we always need to finish the transformation
     // matrix by transposing it as opengl operater
     // on column-major order.
-    t_matrix = MV.mat4.identity();
+    viewMatrix.setLookAt(eye_x, eye_y, eye_z,   // eye points
+                         .0, .0, .0,            // at point
+                         .0, 1., .0);           // up vector
 
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    projMatrix.setOrtho(-1., 1.,      // left, right
+                        -1., 1.,      // bottom, top
+                        near, far); // near, far
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    gl.uniformMatrix4fv(LOCATIONS.u_projMatrix, false, projMatrix.elements);
     gl.uniformMatrix4fv(LOCATIONS.u_viewMatrix, false, viewMatrix.elements);
-    gl.uniformMatrix4fv(LOCATIONS.u_xformMatrix, false, t_matrix);
+    gl.uniformMatrix4fv(LOCATIONS.u_modelMatrix, false, modelMatrix);
 
     // N_VERTICES tells how many times the vertex
     // shader needs to be executed for drawing
     // something.
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, N_VERTICES);
+    gl.drawArrays(gl.TRIANGLES, 0, N_VERTICES);
   }
 
   resize();
   root.addEventListener('resize', resize);
+  document.addEventListener('keydown', (ev) => {
+    console.log("eye (x,y,z)", eye_x, eye_y, eye_z);
+    console.log("near, far: ", near, far);
+
+    switch (ev.key) {
+      case 'ArrowRight':
+        near += .01;
+        break;
+      case 'ArrowLeft':
+        near -= .01;
+        break;
+      case 'ArrowUp':
+        far += .01;
+        break;
+      case 'ArrowDown':
+        far -= .01;
+        break;
+      case 'a':
+        eye_x -= .01;
+        break;
+      case 'd':
+        eye_x += .01;
+        break;
+      case 'w':
+        eye_z -= .01;
+        break;
+      case 's':
+        eye_z += .01;
+        break;
+      default:
+        return;
+    }
+  });
 
   (function loop () {
     root.requestAnimationFrame(loop);
     render();
   })();
-})(window);
+})(window, document);
