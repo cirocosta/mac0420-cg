af1ec89c7b62243d9e385fc3a0397a60d351ef1c
Ciro S. Costa <ciro9758@gmail.com>
2015-05-10 16:44:02 -0300

    renames mesh, adds rotation to the example

--- demos/7-lib/index.js
+++ demos/7-lib/index.js
@@ -7,7 +7,7 @@
  * corpuses. Each of these, if wanting to appear
  * on a camera, must also set up a material for
  * it and blend itself with the material, forming
- * a Mesh. Meshes are added to the world so that
+ * a Inhabitant. Inhabitants are added to the world so that
  * a camera might look at it. In our current
  * world there's only one lightsource (which is a
  * shame).
@@ -18,7 +18,7 @@ import {Renderer} from "../../lib/Renderer";
 import {PerspectiveCamera} from "../../lib/PerspectiveCamera";
 import {Geometry} from "../../lib/Geometry";
 import {Material} from "../../lib/Material";
-import {Mesh} from "../../lib/Mesh";
+import {Inhabitant} from "../../lib/Inhabitant";
 import {ARRAY_BUFFER} from "../../lib/Constants";
 
 const canvas = document.querySelector("canvas");
@@ -44,11 +44,12 @@ let square = new Geometry({
 let material = new Material({
   color: new Float32Array(1.0, 0.0, 0.0)
 });
-let mesh = new Mesh(square, material);
-// mesh.setPosition([0.4, 0.0, 0.0]);
-// mesh.setScale([1.2, 1.0, 1.0]);
+let inhabitant = new Inhabitant(square, material);
+inhabitant.setRotation([0.0, 0.0, 1.0], 90);
+inhabitant.setPosition([0.4, 0.0, 0.0]);
+inhabitant.setScale([1.2, 1.0, 1.0]);
 
-world.add(mesh);
+world.add(inhabitant);
 renderer.render(world, camera);
 
 
